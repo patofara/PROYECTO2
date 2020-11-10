9abc21d42d46ae5e2414052afc556602e576941c
patofara <patriciofara2@gmail.com>
2020-11-10 01:27:29 -0300

    avance

--- HOME/home.js
+++ HOME/home.js
@@ -1,7 +1,170 @@
 // MODO NOCTURNO
 modoNocturno.addEventListener("click", oscurecer)
+modoNocturno.addEventListener("click", oscurecerHome)
+function oscurecerHome() {
+  let lupaExtra = document.getElementsByClassName("lupaExtra")
+    if (document.body.classList.contains('dark')) {
+      lupa[0].src = "/assets/icon-search-mod-noc.svg"
+      lupa2[0].src = "/assets/icon-search-mod-noc.svg"
+      for (let i = 0; i < lupaExtra.length; i++) {
+        lupaExtra[i].src = "/assets/icon-search-mod-noc.svg"
+      }
+    }
+    else{
+      lupa[0].src = "/assets/icon-search.svg"
+      lupa2[0].src = "/assets/icon-search.svg"
+      for (let i = 0; i < lupaExtra.length; i++) {
+        lupaExtra[i].src = "/assets/icon-search.svg"
+      }
+    }
+}
 
 
-  // // HAMBURGUESA 
+
+
+// // HAMBURGUESA 
+
 hamburguesa.addEventListener("click", burger)
 
+
+// BUSQUEDA
+
+
+let search = document.getElementById("search")
+
+search.addEventListener("keyup", (e)=>{
+  if(e.keyCode === 13){
+    
+    busqueda(search.value)
+  }
+});
+
+
+
+
+
+    // PUSHEAR ARRAY DE IMAGENES BUSCADAS
+    
+arrayBuscados = []
+function busqueda (valor){
+  fetch(`http://api.giphy.com/v1/gifs/search?api_key=2Yn9FN3BmE8DqIq2KEG6rApYylEX0ZdQ&q=${valor}&limit=12`)
+  .then(resp => resp.json())
+  .then(resp => {
+    for (let i = 0; i < 12; i++) {
+      let element = resp.data[i].images.downsized.url;
+      arrayBuscados.push(element)
+    }
+    crearDiv();
+  });
+}
+
+    // FUNCION DEL ENTER
+
+search.addEventListener("keyup", buscador)
+let listaSug = document.getElementById("cajaSugerencias")
+let lupa2 = document.getElementsByClassName("lupa2")
+
+function buscador (e){
+  if (search.value != "") {
+    search.style.border = "none"
+    listaSug.classList.add("appear")
+    lupa2[0].classList.add("aparece")
+    lupa[0].src = "/assets/close.svg"
+    cajaSugerencias(search.value)
+  }
+  if (search.value != "" && document.body.classList.contains('dark')) {
+    search.style.border = "none"
+    listaSug.classList.add("appear")
+    lupa2[0].classList.add("aparece")
+    lupa[0].src = "/assets/close-modo-noct.svg"
+    cajaSugerencias(search.value)
+  }
+  if(e.keyCode === 27 || search.value == ""){
+    search.style.border = "1px solid #572EE5"
+    lupa[0].src = "/assets/icon-search.svg"
+    search.value = ""
+    listaSug.classList.remove("appear")
+    lupa2[0].classList.remove("aparece")
+  }
+  if(e.keyCode === 27 || search.value == "" && document.body.classList.contains('dark')){
+    search.style.border = "1px solid white"
+    lupa[0].src = "/assets/icon-search-mod-noc.svg"
+    search.value = ""
+    listaSug.classList.remove("appear")
+    lupa2[0].classList.remove("aparece")
+
+  }
+}
+
+    // Funcion LUPA
+
+let lupa = document.getElementsByClassName("lupa")
+lupa[0].addEventListener("click", () => {
+     listaSug.classList.remove("appear")
+    lupa2[0].classList.remove("aparece")
+    search.style.border = "1px solid #572EE5"
+    lupa[0].src = "/assets/icon-search.svg"
+    search.value = ""
+  })
+      
+    // SUGERENCIAS
+
+let sug1 = document.getElementById("sug1")
+let sug2 = document.getElementById("sug2")
+let sug3 = document.getElementById("sug3")
+let sug4 = document.getElementById("sug4")
+arraySugerencias = [];
+function cajaSugerencias(valor){
+  fetch(`http://api.giphy.com/v1/gifs/search?api_key=2Yn9FN3BmE8DqIq2KEG6rApYylEX0ZdQ&q=${valor}&limit=4`)
+  .then(resp => resp.json())
+  .then(resp => {
+    arraySugerencias = [];
+    for (let i = 0; i < 4; i++) {
+      let element = resp.data[i].user.description;
+      arraySugerencias.push(element)
+      sug1.textContent = arraySugerencias[0]
+      sug2.textContent = arraySugerencias[1]
+      sug3.textContent = arraySugerencias[2]
+      sug4.textContent = arraySugerencias[3]
+      
+    }
+  })
+};
+
+    // CREAR DIV IMAGENES BUSCADAS 
+    
+
+let sectionBuscados = document.getElementById("buscados")
+
+function crearDiv(){
+  let headBuscados = document.createElement("h3")
+  headBuscados.innerHTML = search.value.toUpperCase();
+  sectionBuscados.appendChild(headBuscados)
+  for (let i = 0; i < 12; i++) {
+    let divBuscados = document.createElement("div")
+    let meGusta = document.createElement("img")
+    let descargar = document.createElement("img")
+    let expandir = document.createElement("img")
+    let divOpciones = document.createElement("div")
+    let img = document.createElement("img")
+    let element = arrayBuscados[i];
+    expandir.src= "/assets/icon-max-normal.svg"
+    descargar.src= "/assets/icon-download.svg"
+    meGusta.src = "/assets/icon-fav.svg"
+    divOpciones.appendChild(expandir)
+    divOpciones.appendChild(descargar)
+    divOpciones.appendChild(meGusta)
+    divOpciones.classList.add("opciones")
+    divBuscados.classList.add("divBuscados")
+    img.classList.add("imgBuscadas")
+    img.src = element;
+    divBuscados.appendChild(img)
+    divBuscados.appendChild(divOpciones)
+    sectionBuscados.appendChild(divBuscados)
+  }
+  let verMas = document.createElement("button")
+  verMas.innerHTML = "Ver Mas"
+  sectionBuscados.appendChild(verMas)
+
+  
+}
--- generico.js
+++ generico.js
@@ -1,32 +1,22 @@
-// MODO NOCTURNO
+        // MODO NOCTURNO
 
 let modoNocturno = document.getElementById("modoNocturno");
-let letras = document.querySelectorAll('a,p,h2,h1,h3');
-let fondoSlider = document.getElementById("slider")
+let logo = document.getElementById("logo")
+
 function oscurecer (){
-    let logo = document.getElementById("logo")
-    var storageNoc = sessionStorage.getItem('dark-mode')
+    let storageNoc = sessionStorage.getItem('dark-mode')
     document.body.classList.toggle('dark');
-    fondoSlider.classList.toggle('dark')
-    listaHamb.classList.toggle('dark')
-    
     if(storageNoc === "true"){
-        sessionStorage.setItem('dark-mode','false')
+        sessionStorage.setItem('dark-mode', false)
         modoNocturno.innerHTML = "Modo Nocturno"
         logo.src = "/assets/logo-mobile.svg"
         leftslider.src = "/assets/button-slider-left.svg"
         rightslider.src = "/assets/Button-Slider-right.svg"
         leftslider.addEventListener("mouseleave", () => leftslider.src = "/assets/button-slider-left.svg")
         rightslider.addEventListener("mouseleave", () => rightslider.src = "/assets/Button-Slider-right.svg")
-        for (let i = 0; i < letras.length; i++) {
-            letras[i].classList.toggle('white')
-        }
     }
     
     else{
-        for (let i = 0; i < letras.length; i++) {
-            letras[i].classList.toggle('white')
-        }
         sessionStorage.setItem('dark-mode', true)
         modoNocturno.innerHTML = "Modo Diurno"; 
         logo.src = "/assets/logo-mobile-modo-noct.svg"
@@ -37,9 +27,13 @@ function oscurecer (){
     }
 };
 
+
+
+        // Menu Hamburugesa
+
+
 let hamburguesa = document.getElementById("hamburguesa");
 let listaHamb = document.getElementById("menuNav");
-let masNav = document.getElementById("masNav")
 function burger() {
     listaHamb.classList.toggle('listHamb')
         if(hamburguesa.classList.contains('hamburguesa')){
@@ -54,16 +48,29 @@ function burger() {
         }
 };
 
-// SLIDER TRENDING
 
 
-arrayTrending = [];
-let trending = "http://api.giphy.com/v1/gifs/trending?api_key=2Yn9FN3BmE8DqIq2KEG6rApYylEX0ZdQ&limit=10"
-fetch(trending)
+        // cambia de src con un mouseover y mouseleave
+
+
+function cambiarSrc(elemento,src1,src2) {
+    elemento.addEventListener("mouseover", () => {elemento.src = src1})
+    elemento.addEventListener("mouseleave", () => {elemento.src = src2})
+}
+
+        // SLIDER TRENDING
+
+        // let urlTrendings = fetch ("http://api.giphy.com/v1/gifs/trending?api_key=2Yn9FN3BmE8DqIq2KEG6rApYylEX0ZdQ&limit=12 ")
+        // .then (param =>
+        //     param.json()        
+        // )
+        // .then (param =>
+        //     console.log(param)
+        // )
+let trending = fetch("http://api.giphy.com/v1/gifs/trending?api_key=2Yn9FN3BmE8DqIq2KEG6rApYylEX0ZdQ&limit=12")
 .then(resp => resp.json())
-.then(resp => {
-    console.log(resp);
-    for (let i = 0; i < 10; i++) {
+.then(resp => { 
+    for (let i = 0; i < 12; i++) {
         let element = resp.data[i].images.downsized.url;
         arrayTrending.push(element)
      }
@@ -73,16 +80,20 @@ fetch(trending)
 
     });
 
-
+    
+arrayTrending = [];
 var imagenslider = document.getElementById("imagenslider")
 var imagenslider2 = document.getElementById("imagenslider2")
 var imagenslider3 = document.getElementById("imagenslider3")
 
 
+
+        // contador para el array del carrusel
+
 var contImagenes = 0;
+
 let leftslider = document.getElementById("leftslider")
-leftslider.addEventListener("mouseover", () => leftslider.src = "/assets/button-slider-left-hover.svg")
-leftslider.addEventListener("mouseleave", () => leftslider.src = "/assets/button-slider-left.svg")
+cambiarSrc(leftslider, "/assets/button-slider-left-hover.svg", "/assets/button-slider-left.svg")
 
 leftslider.addEventListener("click", function(){
 
@@ -90,7 +101,9 @@ leftslider.addEventListener("click", function(){
         contImagenes --;
         imagenslider.src = arrayTrending[contImagenes]
         imagenslider2.src = arrayTrending[contImagenes + 1]
-        imagenslider3.src = arrayTrending[contImagenes + 2]    
+        imagenslider3.src = arrayTrending[contImagenes + 2]  
+        meGusta1.src = "/assets/icon-fav.svg";  
+        h++;
     }
     else{
 
@@ -98,8 +111,7 @@ leftslider.addEventListener("click", function(){
 });
 
 let rightslider = document.getElementById("rightslider")
-rightslider.addEventListener("mouseover", () => rightslider.src = "/assets/Button-Slider-right-hover.svg")
-rightslider.addEventListener("mouseleave", () => rightslider.src = "/assets/Button-Slider-right.svg")
+cambiarSrc(rightslider, "/assets/Button-Slider-right-hover.svg", "/assets/Button-Slider-right.svg")
 
 rightslider.addEventListener("click", function(){
     if(contImagenes < (arrayTrending.length - 3)){
@@ -107,12 +119,75 @@ rightslider.addEventListener("click", function(){
         imagenslider2.src = arrayTrending[contImagenes + 2]
         imagenslider3.src = arrayTrending[contImagenes + 3]
         contImagenes ++;
+        meGusta1.src = "/assets/icon-fav.svg";
+        h++;
     }
     else{
 
     }
 });
 
+
+        // Hover de los accesos de los Gifs
+
+var h=1;
+let meGusta1 = document.getElementById("meGusta1")
+meGusta1.addEventListener("mouseover", () => {
+     if (h % 2 != 0) {
+         meGusta1.src = "/assets/icon-fav-hover.svg";
+    }
+    else{
+
+    }
+})
+         
+meGusta1.addEventListener("mouseleave", () => {
+     if (h % 2 != 0) {
+         meGusta1.src = "/assets/icon-fav.svg";
+    }
+    else{
+        
+    }
+})
+meGusta1.addEventListener("click", () => {
     
+    h++;
+    if (h % 2 == 0) {
+    meGusta1.src = "/assets/icon-fav-active.svg";
+    }
+    else{
+        meGusta1.src = "/assets/icon-fav-hover.svg";
+    }
+})
+
+ let megusta2 = document.getElementById("meGusta2")
+ cambiarSrc(megusta2, "/assets/icon-fav-hover.svg", "/assets/icon-fav.svg")
+ let megusta3 = document.getElementById("meGusta3")
+ cambiarSrc(megusta3, "/assets/icon-fav-hover.svg", "/assets/icon-fav.svg")
+ let expandir1 = document.getElementById("expandir1")
+ cambiarSrc(expandir1, "/assets/icon-max-hover.svg", "/assets/icon-max-normal.svg")
+ let expandir2 = document.getElementById("expandir2")
+ cambiarSrc(expandir2, "/assets/icon-max-hover.svg", "/assets/icon-max-normal.svg")
+ let expandir3 = document.getElementById("expandir3")
+ cambiarSrc(expandir3, "/assets/icon-max-hover.svg", "/assets/icon-max-normal.svg")
+ let descargar1 = document.getElementById("descargar1")
+ cambiarSrc(descargar1, "/assets/icon-download-hover.svg", "/assets/icon-download.svg")
+ let descargar2 = document.getElementById("descargar2")
+ cambiarSrc(descargar2, "/assets/icon-download-hover.svg", "/assets/icon-download.svg")
+ let descargar3 = document.getElementById("descargar3")
+ cambiarSrc(descargar3, "/assets/icon-download-hover.svg", "/assets/icon-download.svg")
+        
+ 
+        // Hover de las redes sociales
+
+
+let facebook = document.getElementById("facebook")
+cambiarSrc(facebook, "/assets/icon_facebook_hover.svg", "/assets/icon_facebook.svg")
+let twitter = document.getElementById("twitter")
+cambiarSrc(twitter, "/assets/icon-twitter-hover.svg", "/assets/icon-tw-normal.svg")
+let instagram = document.getElementById("instagram")
+cambiarSrc(instagram, "/assets/icon_instagram-hover.svg", "/assets/icon_instagram.svg")
+
+
 
 
